c944e4711eb1aa2b1b887fb3bb8db4d333e2a992
Rachel Cope <rachel@thoughtbot.com>
2014-12-19 14:44:23 -0700

    Automated commit at 2014-12-19 21:44:23 UTC by middleman-deploy 0.3.0

---
+++ assets/javascripts/buttons.js
@@ -0,0 +1,24 @@
+var calcType = function() {
+  var mbti = '';
+  $('.button1, .button2').each(function(i, obj){
+    if ($(obj).hasClass('active')) {
+      mbti += $(obj).data('letter');
+    }
+  });
+
+  var typeLink = 'http://en.wikipedia.org/wiki/' + mbti;
+  $('.mbti-letters').html(mbti).attr('href', typeLink);
+
+  if (mbti.length === 4){
+    $('.your-type').show();
+  } else {
+    $('.your-type').hide();
+  }
+};
+
+
+$('.button1, .button2').on('click', function() {
+  $(this).parent().parent().children('.button-wrapper').children('.button1, .button2').removeClass('active');
+  $(this).addClass('active');
+  calcType();
+});
